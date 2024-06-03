7617802829a40658c8abba84de55bf3b62eabc8b
Vladislav Cherkasheninov <vladislav.cherkasheninov@taiste.com>
2024-06-03 11:24:49 +0300

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    'react-native/no-inline-styles': 'off',
+    'react-native/no-color-literals': 'off',
+    'react/no-unstable-nested-components': ['off'],
+    'react/react-in-jsx-scope': 'off',
+  },
+};
--- App.tsx
+++ App.tsx
@@ -1,27 +1,27 @@
-import { StatusBar } from "expo-status-bar"
-import { Button, StyleSheet, Text, View } from "react-native"
-import useSpeech from "./src/hooks/useSpeech"
+import {StatusBar} from 'expo-status-bar';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import useSpeech from './src/hooks/useSpeech';
 
 const App = () => {
-	const { speech, startListening } = useSpeech()
+  const {speech, startListening} = useSpeech();
 
-	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<Text>{speech}</Text>
-			<StatusBar style="auto" />
-			<Button onPress={startListening} title="Text to speech" />
-		</View>
-	)
-}
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{speech}</Text>
+      <StatusBar style="auto" />
+      <Button onPress={startListening} title="Text to speech" />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
-export default App
+export default App;
